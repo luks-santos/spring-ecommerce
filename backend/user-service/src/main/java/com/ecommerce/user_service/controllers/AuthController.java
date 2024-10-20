f559e77689a46ff9337239861e614297767aecf8
luks-santos <lucas.bt46@gmail.com>
2024-10-20 15:16:14 -0300

    refactor: enhance validation logic in AuthController and integrate ValidationException Handler

--- backend/user-service/src/main/java/com/ecommerce/user_service/controllers/AuthController.java
+++ backend/user-service/src/main/java/com/ecommerce/user_service/controllers/AuthController.java
@@ -1,24 +1,20 @@
 package com.ecommerce.user_service.controllers;
 
+import com.ecommerce.user_service.dtos.AuthResponseDTO;
 import com.ecommerce.user_service.dtos.UserRegistrationDTO;
 import com.ecommerce.user_service.services.AuthService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,17 +35,11 @@ public class AuthController {
     }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegistrationDTO userRegistrationDto,
-                                          BindingResult bindingResult, HttpServletResponse httpServletResponse) {
+    public ResponseEntity<AuthResponseDTO> registerUser(@RequestBody @Valid UserRegistrationDTO userRegistrationDto,
+                                                        HttpServletResponse httpServletResponse) {
 
         log.info("[AuthController:registerUser]Signup Process Started for user:{}", userRegistrationDto.email());
-        if (bindingResult.hasErrors()) {
-            List<String> errorMessage = bindingResult.getAllErrors().stream()
-                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                    .toList();
-            log.error("[AuthController:registerUser]Errors in user:{}", errorMessage);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
-        }
-        return ResponseEntity.ok(service.registerUser(userRegistrationDto, httpServletResponse));
+        AuthResponseDTO response = service.registerUser(userRegistrationDto, httpServletResponse);
+        return ResponseEntity.ok(response);
     }
 }
--- backend/user-service/src/main/java/com/ecommerce/user_service/dtos/UserRegistrationDTO.java
+++ backend/user-service/src/main/java/com/ecommerce/user_service/dtos/UserRegistrationDTO.java
@@ -2,23 +2,26 @@ package com.ecommerce.user_service.dtos;
 
 import com.ecommerce.user_service.enums.UserRole;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 
+@Builder
 public record UserRegistrationDTO(
-        @NotEmpty(message = "User first name must not be empty")
+        @NotBlank(message = "User first name must not be empty")
         String firstName,
-        @NotEmpty(message = "User last name must not be empty")
+        @NotBlank(message = "User last name must not be empty")
         String lastName,
         @Email(message = "Invalid email format")
-        @NotEmpty(message = "User email must not be empty")
+        @NotBlank(message = "User email must not be empty")
         String email,
-        @NotEmpty(message = "User phone must not be empty")
+        @NotBlank(message = "User phone must not be empty")
         String phone,
-        @NotEmpty(message = "User address must not be empty")
+        @NotBlank(message = "User address must not be empty")
         String address,
-        @NotEmpty(message = "User password must not be empty")
-        String password,
-        @NotNull(message = "User role must not be null")
-        UserRole role) {
+        @NotBlank(message = "User password must not be empty")
+        String password
+        ) {
 }

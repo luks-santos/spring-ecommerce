7fc6fb75d2702993ca96b753405c5a54a595b735
luks-santos <lucas.bt46@gmail.com>
2024-10-20 12:23:31 -0300

    test: add integration tests for updating user with valid and invalid data requests

--- backend/user-service/src/test/java/com/ecommerce/user_service/controllers/UserControllerTest.java
+++ backend/user-service/src/test/java/com/ecommerce/user_service/controllers/UserControllerTest.java
@@ -1,16 +1,21 @@
 package com.ecommerce.user_service.controllers;
 
 import com.ecommerce.user_service.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.ecommerce.user_service.common.UserConstants.INVALID_USER_DTO;
 import static com.ecommerce.user_service.common.UserConstants.USER_DTO;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,9 +25,11 @@ class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private UserService userService;
-
 
     @Test
     public void getLoggedUser_WithValidRequest_ReturnsOkWithUserDTO() throws Exception {
@@ -30,9 +37,36 @@ class UserControllerTest {
 
         mockMvc.perform(get("/api/account/logged-user")
                         .with(SecurityMockMvcRequestPostProcessors.jwt()))
+                .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.firstName").value("First Name"))
-                .andExpect(jsonPath("$.lastName").value("Last Name"))
-                .andExpect(jsonPath("$.email").value("user@example.com"));
+                .andExpect(jsonPath("$").value(USER_DTO));
+    }
+
+    @Test
+    public void update_WithValidData_ReturnsOkWithUserDTO() throws Exception {
+        when(userService.update(USER_DTO)).thenReturn(USER_DTO);
+
+        mockMvc.perform(put("/api/account/update_profile")
+                        .content(objectMapper.writeValueAsString(USER_DTO)).contentType(MediaType.APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors.jwt()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(USER_DTO));
+    }
+
+    @Test
+    public void update_WithInValidData_ReturnsOkWithUserDTO() throws Exception {
+        when(userService.update(INVALID_USER_DTO)).thenReturn(INVALID_USER_DTO);
+
+        mockMvc.perform(put("/api/account/update_profile")
+                        .content(objectMapper.writeValueAsString(INVALID_USER_DTO)).contentType(MediaType.APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors.jwt()))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.firstName").value("User first name must not be empty"))
+                .andExpect(jsonPath("$.lastName").value("User last name must not be empty"))
+                .andExpect(jsonPath("$.email").value("Invalid email format"))
+                .andExpect(jsonPath("$.phone").value("User phone must not be empty"))
+                .andExpect(jsonPath("$.address").value("User address must not be empty"));
     }
 }
